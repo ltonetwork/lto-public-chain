251bfa4e56dce977c3f746d4fb569cecb6ad5013
Ilya Smagin <ilya.smagin@gmail.com>
2018-10-03 18:31:53 +0300

    fix leading zeros addition@

--- src/test/scala/com/wavesplatform/transaction/TransactionFactoryTest.scala
+++ src/test/scala/com/wavesplatform/transaction/TransactionFactoryTest.scala
@@ -5,20 +5,6 @@ import org.scalatest.prop.PropertyChecks
 import org.scalatest.{Matchers, PropSpec}
 
 class TransactionFactoryTest extends PropSpec with PropertyChecks with Matchers with TransactionGen with NoShrink {
-  property("accepts 64/128/256/384/512") {
-    val lstGen = for {
-      a64  <- byteArrayGen(64)
-      a128 <- byteArrayGen(128)
-      a256 <- byteArrayGen(256)
-      a384 <- byteArrayGen(384)
-      a512 <- byteArrayGen(512)
-    } yield List(a64, a128, a256, a384, a512).map(ByteStr(_).toString)
-
-    forAll(lstGen) { l =>
-      TransactionFactory.parseAnchors(l) shouldBe 'right
-    }
-  }
-
   property("accepts < 64, prepends zeros") {
     forAll(genBoundedBytes(0, 64)) { b =>
       {
