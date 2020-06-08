e9becc368c46316c327015937f15b94e5f5da3ab
D. Vandervoorde <d.vandervoorde@gmail.com>
2020-06-08 19:19:24 +0300

    fee burn feature introduced

--- src/main/scala/com/wavesplatform/features/BlockchainFeature.scala
+++ src/main/scala/com/wavesplatform/features/BlockchainFeature.scala
@@ -10,11 +10,14 @@ object BlockchainFeatures {
   val SmartAccounts                                   = BlockchainFeature(4, "Smart Accounts")
   val AssociationTransaction                          = BlockchainFeature(10, "Association Transaction")
   val SponsorshipTransaction                          = BlockchainFeature(11, "Sponsorship Transaction")
+  val BurnFeeture = BlockchainFeature(12, "Partial Fee Burn")
+
 
   private val dict = Seq(
     SmartAccounts,
     AssociationTransaction,
-    SponsorshipTransaction
+    SponsorshipTransaction,
+    BurnFeeture
   ).map(f => f.id -> f).toMap
 
   private val preActivated: Set[Short] = Set(1, 2, 3, 5, 8).map(_.toShort) // consensus logic hardcoded
