34e1bb5ae110f9bbae92b813f1dbd353a4c89b5e
Arnold Daniels <arnold@jasny.net>
2021-03-25 14:13:46 +0100

    Keep 64 byte check for multi anchor tx.

--- src/main/scala/com/ltonetwork/state/diffs/AnchorTransactionDiff.scala
+++ src/main/scala/com/ltonetwork/state/diffs/AnchorTransactionDiff.scala
@@ -9,10 +9,12 @@ object AnchorTransactionDiff {
   import com.ltonetwork.features.FeatureProvider._
   def apply(blockchain: Blockchain, height: Int)(tx: AnchorTransaction): Either[ValidationError, Diff] = {
     (
-      if (
-        !blockchain.isFeatureActivated(BlockchainFeatures.TransactionsV3, height) &&
-        blockchain.isFeatureActivated(BlockchainFeatures.AssociationTransaction, height)
-      ) for {
+      if (blockchain.isFeatureActivated(BlockchainFeatures.TransactionsV3, height))
+        for {
+          _ <- Either.cond(tx.anchors.head.arr.length <= AnchorTransaction.NewMaxEntryLength, (), GenericError("Anchor should contain <= 64 bytes"))
+        } yield ()
+      else if (blockchain.isFeatureActivated(BlockchainFeatures.AssociationTransaction, height))
+        for {
          _ <- Either.cond(tx.anchors.size == 1, (), GenericError("AnchorTransaction should have exactly 1 anchor"))
          _ <- Either.cond(tx.anchors.head.arr.length <= AnchorTransaction.NewMaxEntryLength, (), GenericError("Anchor should contain <= 64 bytes"))
         } yield ()
