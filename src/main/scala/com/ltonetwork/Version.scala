b38b0163247662284bc265280bd4aeca920ec858
Arnold Daniels <arnold@jasny.net>
2021-05-14 18:29:58 +0200

    Use Version.scala as fallback instead of fallback values in build.sbt.

---
+++ src/main/scala/com/ltonetwork/Version.scala
@@ -0,0 +1,10 @@
+package com.ltonetwork
+
+// Please, update the fallback version every major and minor releases.
+// This version is used then building from sources without Git repository
+// In case of not updating the version nodes build from headless sources will fail to connect to newer versions
+
+object Version {
+  val VersionString = "${version.value}"
+  val VersionTuple: (Int, Int, Int) = (1, 3, 0)
+}
