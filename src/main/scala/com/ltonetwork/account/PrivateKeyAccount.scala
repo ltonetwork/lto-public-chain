24ee6f72152a726cfb7c62acfa7c416ac0abdbdd
Kaloyan Tanev <tanevdev@gmail.com>
2021-07-13 12:59:28 +0200

    Add crypto tests

--- src/main/scala/com/ltonetwork/account/PrivateKeyAccount.scala
+++ src/main/scala/com/ltonetwork/account/PrivateKeyAccount.scala
@@ -19,14 +19,22 @@ object PrivateKeyAccount {
   private case class PrivateKeyAccountImpl(seed: Array[Byte], keyType: KeyType, privateKey: Array[Byte], publicKey: Array[Byte])
       extends PrivateKeyAccount
 
-  def apply(seed: Array[Byte]): PrivateKeyAccount = {
-    val pair = crypto.createKeyPair(seed)
-    PrivateKeyAccountImpl(seed, ED25519, pair._1, pair._2)
+  def apply(seed: Array[Byte], keyType: KeyType): PrivateKeyAccount = {
+    val pair = crypto.createKeyPair(seed, keyType)
+    PrivateKeyAccountImpl(seed, keyType, pair._1, pair._2)
   }
 
-  def fromSeed(s: String): Either[GenericError, PrivateKeyAccount] = Base58.decode(s) match {
-    case Success(x) => Right(PrivateKeyAccount(x))
+  def apply(seed: Array[Byte]): PrivateKeyAccount = {
+    apply(seed, ED25519)
+  }
+
+  def fromSeed(s: String, keyType: KeyType): Either[GenericError, PrivateKeyAccount] = Base58.decode(s) match {
+    case Success(x) => Right(PrivateKeyAccount(x, keyType))
     case Failure(e) => Left(GenericError(s"Unable to get a private key from the seed '$s': ${e.getMessage}"))
+  }
+
+  def fromSeed(s: String): Either[GenericError, PrivateKeyAccount] = {
+    fromSeed(s, ED25519)
   }
 
 }
