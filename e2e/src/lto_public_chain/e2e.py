15623c7ae6d9702e2b8e325fb16b1da8154feada
Kaloyan Tanev <tanevdev@gmail.com>
2021-08-16 17:55:28 +0200

    WIP: atomic_swap test

--- e2e/src/lto_public_chain/e2e.py
+++ e2e/src/lto_public_chain/e2e.py
@@ -2,6 +2,7 @@ import unittest
 
 import config
 import http_requests
+import utils
 
 class E2eTests(unittest.TestCase):
     def test_connectivity(self):
@@ -9,9 +10,19 @@ class E2eTests(unittest.TestCase):
             http_requests.get("/").status_code,
             200)
 
+    def atomic_swap(self):
+        alice_address = utils.create_wallet().json()['address']
+        bob_address = utils.create_wallet().json()['address']
+        swap_address = utils.create_wallet().json()['address']
+        acc = utils.create_account('bEaMXTP5K1bm3uH8AKXJeBfHBP6TBP2dexxHzbEiqXHADmT3mrLaTUjGdK1XRXayMjdimB3kXPUd8pACPKuWAqt7iEohZyhHAuCnxBRqZbXw9VmdaRmvtoNu3SBRCswG4wC')
+        # self.assertEqual(
+        #     http_requests.get("/debug/minerInfo").status_code,
+        #     200)
+
 def run():
     suite = unittest.TestSuite()
     suite.addTest(E2eTests("test_connectivity"))
+    suite.addTest(E2eTests("atomic_swap"))
     runner = unittest.TextTestRunner(verbosity=2)
     result = runner.run(suite)
     assert result.wasSuccessful()
