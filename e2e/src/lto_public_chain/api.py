610c97c01d4be7a4fe7771d0734edafad7199f8e
Kaloyan Tanev <tanevdev@gmail.com>
2021-08-18 17:01:58 +0200

    Add invoke and revoke association test

--- e2e/src/lto_public_chain/api.py
+++ e2e/src/lto_public_chain/api.py
@@ -1,3 +1,7 @@
+import string
+import polling
+import random
+
 import config
 import http_requests
 import PyCLTO
@@ -16,12 +20,28 @@ def transfer(sender, recipient, amount, attachment=''):
     sender_account = create_account(sender.seed)
     return sender_account.sendLTO(recipient, amount, attachment)
 
+def invoke_association(sender, party, anchor, association_type=1):
+    sender_account = create_account(sender.seed)
+    return sender_account.invokeAssociation(party, association_type, anchor)
+
+def revoke_association(sender, party, anchor, association_type=1):
+    sender_account = create_account(sender.seed)
+    return sender_account.revokeAssociation(party, association_type, anchor)
+
+def list_associations(address):
+    return http_requests.get("/associations/status/{}".format(address))
+
 def set_script(sender, script):
     sender_account = create_account(sender.seed)
     return sender_account.setScript(script)
 
 def get_tx(id):
-    return pl.tx(id)
+    return polling.poll(
+        lambda: pl.tx(id),
+        check_success=lambda response: 'id' in response,
+        step=1,
+        poll_forever=True
+    )
 
 def get_height():
     return pl.height()
--- e2e/src/lto_public_chain/e2e.py
+++ e2e/src/lto_public_chain/e2e.py
@@ -4,6 +4,7 @@ import time
 import config
 import http_requests
 import api
+import utils
 
 class E2eTests(unittest.TestCase):
     faucet = api.create_account('cable sniff exchange judge gym rifle prevent traffic picture firm shaft exist cute unusual check')
@@ -15,6 +16,99 @@ class E2eTests(unittest.TestCase):
         self.assertEqual(
             http_requests.get("/").status_code,
             200)
+
+    # Scenario:
+    # 1. Alice gets associated with Bob
+    # 2. Alice revokes her association with Bob
+    def test_invoke_and_revoke_association(self):
+        anchor = utils.random_string()
+
+        # Step 1: Alice gets associated with Bob
+        invoke_assoc_tx = api.invoke_association(self.alice, self.bob, anchor)
+        invoke_assoc_tx_id = invoke_assoc_tx['id']
+        polled_invoke_assoc_tx = api.get_tx(invoke_assoc_tx_id)
+
+        self.assertEqual(
+            polled_invoke_assoc_tx['id'],
+            invoke_assoc_tx_id)
+
+        alice_assocs = api.list_associations(self.alice.address).json()
+        alice_assoc = next((assoc for assoc in alice_assocs['outgoing'] if assoc['issueTransactionId'] == invoke_assoc_tx_id), None)
+
+        self.assertIsNotNone(alice_assoc)
+        self.assertEqual(
+            alice_assoc['associationType'],
+            1
+        )
+        self.assertEqual(
+            alice_assoc['party'],
+            self.bob.address
+        )
+        self.assertFalse('revokeTransactionId' in alice_assoc)
+        self.assertFalse('revokeHeight' in alice_assoc)
+
+        bob_assocs = api.list_associations(self.bob.address).json()
+        bob_assoc = next((assoc for assoc in bob_assocs['incoming'] if assoc['issueTransactionId'] == invoke_assoc_tx_id), None)
+
+        self.assertIsNotNone(bob_assoc)
+        self.assertEqual(
+            bob_assoc['associationType'],
+            1
+        )
+        self.assertEqual(
+            bob_assoc['party'],
+            self.alice.address
+        )
+        self.assertEqual(
+            polled_invoke_assoc_tx['id'],
+            invoke_assoc_tx['id'])
+        self.assertFalse('revokeTransactionId' in bob_assoc)
+        self.assertFalse('revokeHeight' in bob_assoc)
+
+        # Step 2: Alice revokes her association with Bob
+
+        revoke_assoc_tx = api.revoke_association(self.alice, self.bob, anchor)
+        revoke_assoc_tx_id = revoke_assoc_tx['id']
+        polled_revoke_assoc_tx = api.get_tx(revoke_assoc_tx_id)
+
+        self.assertEqual(
+            polled_revoke_assoc_tx['id'],
+            revoke_assoc_tx_id)
+
+        alice_assocs_after_revoke = api.list_associations(self.alice.address).json()
+        alice_assoc_after_revoke = next((assoc for assoc in alice_assocs_after_revoke['outgoing'] if 'revokeTransactionId' in assoc and assoc['revokeTransactionId'] == revoke_assoc_tx_id), None)
+
+        self.assertIsNotNone(alice_assoc_after_revoke)
+        self.assertEqual(
+            alice_assoc_after_revoke['issueTransactionId'],
+            alice_assoc['issueTransactionId']
+        )
+        self.assertEqual(
+            alice_assoc_after_revoke['associationType'],
+            alice_assoc['associationType']
+        )
+        self.assertEqual(
+            alice_assoc_after_revoke['party'],
+            self.bob.address
+        )
+        self.assertTrue(alice_assoc_after_revoke['revokeHeight'] >= alice_assoc_after_revoke['issueHeight'])
+
+        bob_assocs = api.list_associations(self.bob.address).json()
+        bob_assoc = next((assoc for assoc in bob_assocs['incoming'] if assoc['issueTransactionId'] == revoke_assoc_tx_id), None)
+
+        self.assertIsNotNone(bob_assoc)
+        self.assertEqual(
+            bob_assoc['associationType'],
+            1
+        )
+        self.assertEqual(
+            bob_assoc['party'],
+            self.alice.address
+        )
+        self.assertEqual(
+            polled_invoke_assoc_tx['id'],
+            invoke_assoc_tx['id'])
+
 
     # Scenario:
     # 1. Alice's and Charlie's balances initialisation
@@ -50,7 +144,7 @@ class E2eTests(unittest.TestCase):
         print(script)
 
         set_script_tx = api.set_script(self.charlie, script)
-        
+
         # self.assertEqual(
         #     http_requests.get("/debug/minerInfo").status_code,
         #     200)
@@ -58,7 +152,7 @@ class E2eTests(unittest.TestCase):
 def run():
     suite = unittest.TestSuite()
     suite.addTest(E2eTests("test_connectivity"))
-    suite.addTest(E2eTests("atomic_swap"))
+    suite.addTest(E2eTests("test_invoke_and_revoke_association"))
     runner = unittest.TextTestRunner(verbosity=2)
     result = runner.run(suite)
     assert result.wasSuccessful()
