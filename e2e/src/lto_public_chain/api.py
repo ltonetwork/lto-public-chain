0b9a60557029156894e206c5608e91e27e825e01
Kaloyan Tanev <tanevdev@gmail.com>
2021-08-20 02:05:50 +0200

    Lease test

--- e2e/src/lto_public_chain/api.py
+++ e2e/src/lto_public_chain/api.py
@@ -1,6 +1,8 @@
-import string
+import subprocess
+import time
+
+import requests
 import polling
-import random
 
 import config
 import http_requests
@@ -31,11 +33,22 @@ def revoke_association(sender, party, anchor, association_type=1):
 def list_associations(address):
     return http_requests.get("/associations/status/{}".format(address))
 
+def lease(lessor, lessee, amount):
+    sender_account = create_account(lessor.seed)
+    return sender_account.lease(lessee, amount)
+
+def cancel_lease(lessor, lease_id):
+    sender_account = create_account(lessor.seed)
+    return sender_account.leaseCancel(lease_id)
+
+def list_active_leases(address):
+    return http_requests.get("/leasing/active/{}".format(address))
+
 def set_script(sender, script):
     sender_account = create_account(sender.seed)
     return sender_account.setScript(script)
 
-def get_tx(id):
+def get_tx_polled(id):
     return polling.poll(
         lambda: pl.tx(id),
         check_success=lambda response: 'id' in response,
@@ -45,3 +58,29 @@ def get_tx(id):
 
 def get_height():
     return pl.height()
+
+def shutdown_node():
+    http_requests.post("/node/stop")
+
+def start_node():
+    subprocess.call("bin/run_public_node", shell=True)
+
+def is_node_down():
+    try:
+        polling.poll(
+            lambda: http_requests.get("/"),
+            check_success=lambda response: response.status_code != 200,
+            step=1,
+            timeout=180
+        )
+        return False
+    except requests.exceptions.ConnectionError:
+        return True
+
+def is_node_up():
+    return polling.poll(
+        lambda: http_requests.get("/").status_code == 200,
+        step=1,
+        ignore_exceptions=(requests.exceptions.ConnectionError),
+        timeout=180
+    )
---
+++ e2e/src/lto_public_chain/api_external.py
@@ -0,0 +1,41 @@
+import subprocess
+import time
+
+import requests
+import polling
+
+import config
+import http_requests
+import PyCLTO
+
+pl = PyCLTO.PyCLTO()
+pl.NODE = "https://testnet.lto.network"
+pl.setChain("testnet")
+
+def create_account(base58_seed):
+    return pl.Address(seed=base58_seed)
+
+def cancel_lease(address, lease_id):
+    sender_account = create_account(address.seed)
+    return sender_account.leaseCancel(lease_id)
+
+def get_tx_polled(id):
+    return polling.poll(
+        lambda: pl.tx(id),
+        check_success=lambda response: 'id' in response,
+        step=1,
+        timeout=60
+    )
+
+def is_lease_missing(address, lease_id):
+    try:
+        polling.poll(
+            lambda: http_requests.get_from_url("https://testnet.lto.network/leasing/active/{}".format(address)),
+            check_success=lambda response: (not any(r['id'] == lease_id for r in response.json())),
+            ignore_exceptions=(requests.exceptions.ConnectionError),
+            step=1,
+            timeout=60
+        )
+        return True
+    except polling.TimeoutException:
+        return False
