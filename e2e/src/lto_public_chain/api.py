7d23b5e275dc8da5805417e5aea5ee7109058239
Andrea Scorza <atlantas95@gmail.com>
2021-10-13 10:02:33 +0200

    porting api

--- e2e/src/lto_public_chain/api.py
+++ e2e/src/lto_public_chain/api.py
@@ -6,33 +6,60 @@ import polling
 
 import config
 import http_requests
-import PyCLTO
+from LTO import PyCLTO
+from LTO.Accounts.AccountFactoryED25519 import AccountED25519
+from LTO.Transactions.Transfer import Transfer
+from LTO.Transactions.Association import Association
+from LTO.Transactions.RevokeAssociation import RevokeAssociation
+from LTO.Transactions.Lease import Lease
+from LTO.Transactions.CancelLease import CancelLease
+from LTO.PublicNode import PublicNode
+from LTO.Transactions.SetScript import SetScript
+from LTO.Transactions.MassTransfer import MassTransfer
+from LTO.Transactions.Sponsorship import Sponsorship
+from LTO.Transactions.CancelSponsorship import CancelSponsorship
+from LTO.Transactions.Anchor import Anchor
 
-pl = PyCLTO.PyCLTO()
-pl.NODE = config.node_url
-pl.setChain("testnet")
+
+CHAIN_ID = 'T'
+
+
+pl = PyCLTO(CHAIN_ID)
+#pl.NODE = config.node_url
+#pl.setChain("testnet")
 
 def create_wallet():
     return http_requests.post("/addresses", "")
 
+
 def create_account(base58_seed):
-    return pl.Address(seed=base58_seed)
+    account = AccountED25519(CHAIN_ID).createFromSeed(base58_seed)
+    return account.address
+    #return pl.Address(seed=base58_seed)
 
 def transfer(sender, recipient, amount, attachment=''):
     sender_account = create_account(sender.seed)
     return sender_account.sendLTO(recipient, amount, attachment)
 
 def transfer_v3(sender, recipient, amount, attachment=''):
-    sender_account = create_account(sender.seed)
-    return sender_account.sendLTOv3(recipient, amount, attachment)
+    sender_account = AccountED25519(CHAIN_ID).createFromSeed(sender.seed)
+    return Transfer(recipient=recipient, amount=amount, attachment=attachment).signWith(sender_account)
 
 def invoke_association(sender, party, anchor, association_type=1):
     sender_account = create_account(sender.seed)
     return sender_account.invokeAssociation(party, association_type, anchor)
 
+def invoke_association_v3(sender, party, anchor, association_type=1):
+    sender_account = AccountED25519(CHAIN_ID).createFromSeed(sender.seed)
+    return Association(party, association_type, anchor).signWith(sender_account)
+
 def revoke_association(sender, party, anchor, association_type=1):
     sender_account = create_account(sender.seed)
     return sender_account.revokeAssociation(party, association_type, anchor)
+
+def revoke_association_v3(sender, party, anchor, association_type=1):
+    sender_account = AccountED25519(CHAIN_ID).createFromSeed(sender.seed)
+    return RevokeAssociation(party, association_type, anchor).signWith(sender_account)
 
 def list_associations(address):
     return http_requests.get("/associations/status/{}".format(address))
@@ -42,51 +69,59 @@ def lease(lessor, lessee, amount):
     return sender_account.lease(lessee, amount)
 
 def lease_v3(lessor, lessee, amount):
-    sender_account = create_account(lessor.seed)
-    return sender_account.leasev3(lessee, amount)
+    sender_account = AccountED25519(CHAIN_ID).createFromSeed(lessor.seed)
+    return Lease(lessee, amount).signWith(sender_account)
 
 def cancel_lease(lessor, lease_id):
     sender_account = create_account(lessor.seed)
     return sender_account.leaseCancel(lease_id)
 
 def cancel_lease_v3(lessor, lease_id):
-    sender_account = create_account(lessor.seed)
-    return sender_account.leaseCancelv3(lease_id)
+    sender_account = AccountED25519(CHAIN_ID).createFromSeed(lessor.seed)
+    return CancelLease(lease_id).signWith(sender_account)
 
 def list_active_leases(address):
-    return http_requests.get("/leasing/active/{}".format(address))
+     return PublicNode(pl.NODE.url).leaseList(address)
 
 def set_script(sender, script):
     sender_account = create_account(sender.seed)
     return sender_account.setScript(script)
 
+def set_script_v3(sender, script):
+    sender_account = AccountED25519(CHAIN_ID).createFromSeed(sender.seed)
+    return SetScript(script).signWith(sender_account)
+
 def mass_transfer(sender, transfers, attachment='e2etests'):
     sender_account = create_account(sender.seed)
     return sender_account.massTransferLTO(transfers, attachment)
+
+def mass_transfer_v3(sender, transfers, attachment='e2etests'):
+    sender_account = AccountED25519(CHAIN_ID).createFromSeed(sender.seed)
+    return MassTransfer(transfers, attachment).signWith(sender_account)
 
 def sponsor(sponsor, party):
     sender_account = create_account(sponsor.seed)
     return sender_account.sponsor(party)
 
-def sponsor_v3(sponsor, party):
-    sender_account = create_account(sponsor.seed)
-    return sender_account.sponsorv3(party)
+def sponsor_v3(sponsor, recipient):
+    sender_account = AccountED25519(CHAIN_ID).createFromSeed(sponsor.seed)
+    return Sponsorship(recipient).signWith(sender_account)
 
 def cancel_sponsor(sponsor, party):
     sender_account = create_account(sponsor.seed)
     return sender_account.cancelSponsor(party)
 
-def cancel_sponsor_v3(sponsor, party):
-    sender_account = create_account(sponsor.seed)
-    return sender_account.cancelSponsorv3(party)
+def cancel_sponsor_v3(sponsor, recipient):
+    sender_account = AccountED25519(CHAIN_ID).createFromSeed(sponsor.seed)
+    return CancelSponsorship(recipient).signWith(sender_account)
 
 def anchor(sender, hash):
     sender_account = create_account(sender.seed)
     return sender_account.anchor(hash)
 
 def anchor_v3(sender, hash):
-    sender_account = create_account(sender.seed)
-    return sender_account.anchorv3(hash)
+    sender_account = AccountED25519(CHAIN_ID).createFromSeed(sender.seed)
+    return Anchor(hash).signWith(sender_account)
 
 def get_tx_polled(id):
     return polling.poll(
@@ -97,10 +132,10 @@ def get_tx_polled(id):
     )
 
 def get_height():
-    return pl.height()
+    return PublicNode(pl.NODE.url).height()
 
 def get_address_balance(address):
-    return http_requests.get("/addresses/balance/details/{}".format(address))
+    return PublicNode(pl.NODE.url).balance(address)
 
 def shutdown_node():
     http_requests.post("/node/stop")
