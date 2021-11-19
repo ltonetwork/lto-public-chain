a540d9f39596ba338296c3bdc0ef864f765cf3f0
AndreaScorza <atlantas95@gmail.com>
2021-11-19 17:27:46 +0100

    added control to stop the node just if it wasn't running before

--- e2e/environment.py
+++ e2e/environment.py
@@ -1,10 +1,16 @@
 from e2e.common import node
-from behave import *
+
+NODE_RUNNING = False
+
 
 def before_all(context):
+    global NODE_RUNNING
     if node.is_node_up() is False:
         node.start_node()
+    else:
+        NODE_RUNNING = True
 
 
 def after_all(context):
-    node.stop_node()
+    if NODE_RUNNING is False:
+        node.stop_node()
