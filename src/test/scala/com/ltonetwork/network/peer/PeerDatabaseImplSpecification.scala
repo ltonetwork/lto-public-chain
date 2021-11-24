e730c1be9ad8792c1320cc0d5ae0b5aa439f812a
Kaloyan Tanev <tanevdev@gmail.com>
2021-11-24 19:55:45 +0100

    Change AnyFreeSpecLike to PathAnyFreeSpecLike (isolating test cases)

--- src/test/scala/com/ltonetwork/network/peer/PeerDatabaseImplSpecification.scala
+++ src/test/scala/com/ltonetwork/network/peer/PeerDatabaseImplSpecification.scala
@@ -8,10 +8,10 @@ import com.typesafe.config.ConfigFactory
 import com.ltonetwork.network.PeerDatabaseImpl
 import com.ltonetwork.settings.NetworkSettings
 import net.ceedubs.ficus.Ficus._
-import org.scalatest.freespec.AnyFreeSpecLike
+import org.scalatest.freespec.PathAnyFreeSpecLike
 import org.scalatest.matchers.should.Matchers
 
-class PeerDatabaseImplSpecification extends AnyFreeSpecLike with Matchers {
+class PeerDatabaseImplSpecification extends PathAnyFreeSpecLike with Matchers {
 
   private val config1   = ConfigFactory.parseString("""lto.network {
       |  file = null
