9d28ae4475fc7fd2447b99b094eb0a86b0b32b05
Kaloyan Tanev <tanevdev@gmail.com>
2021-12-14 23:54:28 +0200

    WIP: Add RegisterTransactionSpecification

---
+++ src/test/scala/com/ltonetwork/transaction/RegisterTransactionSpecification.scala
@@ -0,0 +1,159 @@
+package com.ltonetwork.transaction
+
+import com.ltonetwork.TransactionGen
+import com.ltonetwork.account.PublicKeyAccount
+import com.ltonetwork.api.http.requests.RegisterRequest
+import com.ltonetwork.state.{ByteStr, EitherExt2}
+import com.ltonetwork.transaction.register.RegisterTransaction
+import com.ltonetwork.utils.Base58
+import org.scalatest.Assertion
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.propspec.AnyPropSpec
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+import play.api.libs.json.Json
+
+class RegisterTransactionSpecification extends AnyPropSpec with ScalaCheckDrivenPropertyChecks with TableDrivenPropertyChecks with Matchers with TransactionGen {
+
+  private def checkSerialization(tx: RegisterTransaction): Assertion = {
+    val tryParse = RegisterTransaction.parseBytes(tx.bytes())
+    tryParse should be a 'success
+
+    val parsed = tryParse.get
+    parsed.sender.address shouldEqual tx.sender.address
+    parsed.timestamp shouldEqual tx.timestamp
+    parsed.fee shouldEqual tx.fee
+    parsed.sponsor shouldEqual tx.sponsor
+
+    parsed.keys.zip(tx.keys).foreach {
+      case (r, t) =>
+        r shouldEqual t
+    }
+
+    parsed.bytes() shouldEqual tx.bytes()
+  }
+
+  property("serialization roundtrip versions") {
+    forEvery(versionTable(RegisterTransaction)) { version: Byte =>
+      forAll(registerTransactionGen(version))(checkSerialization)
+    }
+  }
+
+  property("serialization roundtrip keypairs") {
+    forEvery(keyTypeTable) { keyType =>
+      forAll(registerTransactionGen(3.toByte, keyType))(checkSerialization)
+    }
+  }
+
+  property("from TransactionBuilder") {
+    forAll(registerTransactionGen) { tx: RegisterTransaction =>
+      val recovered = TransactionBuilders.parseBytes(tx.bytes()).get
+      recovered.bytes() shouldEqual tx.bytes()
+    }
+  }
+
+  property("JSON roundtrip") {
+    forAll(registerTransactionGen) { tx =>
+      val json = tx.json()
+      json.toString shouldEqual tx.toString
+
+      val req = json.as[RegisterRequest]
+      req.senderPublicKey should be('defined)
+      req.senderPublicKey.get shouldEqual Base58.encode(tx.sender.publicKey)
+      req.fee shouldEqual tx.fee
+      req.timestamp should be('defined)
+      req.timestamp.get shouldEqual tx.timestamp
+
+      req.keys zip tx.keys foreach {
+        case (re, te) =>
+          re shouldEqual te
+      }
+    }
+  }
+
+  //TODO: Will fail, get correct proof.
+  property(testName = "JSON format validation") {
+    val js = Json.parse("""{
+                       "type": 20,
+                       "version": 3,
+                       "id": "Aa5LusYsEDYVkWozd1BYRLVqbwWeJid3xiBrd2KetM5j",
+                       "sender": "3Mr31XDsqdktAdNQCdSd8ieQuYoJfsnLVFg",
+                       "senderKeyType": "ed25519",
+                       "senderPublicKey": "FM5ojNqW7e9cZ9zhPYGkpSP1Pcd8Z3e3MNKYVS5pGJ8Z",
+                       "fee": 100000,
+                       "timestamp": 1526911531530,
+                       "keys": [
+                         "D6HmGZqpXCyAqpz8mCAfWijYDWsPKncKe5v3jq1nTpf5"
+                       ],
+                       "proofs": [
+                         "32mNYSefBTrkVngG5REkmmGAVv69ZvNhpbegmnqDReMTmXNyYqbECPgHgXrX2UwyKGLFS45j7xDFyPXjF8jcfw94"
+                       ]
+                       }
+  """)
+
+    val key = PublicKeyAccount.fromBase58String("FM5ojNqW7e9cZ9zhPYGkpSP1Pcd8Z3e3MNKYVS5pGJ8Z").explicitGet()
+    val proof  = ByteStr.decodeBase58("32mNYSefBTrkVngG5REkmmGAVv69ZvNhpbegmnqDReMTmXNyYqbECPgHgXrX2UwyKGLFS45j7xDFyPXjF8jcfw94").get
+
+    val tx = RegisterTransaction
+      .create(
+        3,
+        None,
+        1526911531530L,
+        PublicKeyAccount.fromBase58String("FM5ojNqW7e9cZ9zhPYGkpSP1Pcd8Z3e3MNKYVS5pGJ8Z").explicitGet(),
+        100000,
+        List(key),
+        None,
+        Proofs(Seq(proof))
+      )
+      .explicitGet()
+
+    tx.json() shouldEqual js
+  }
+
+  //TODO: Will fail, get correct proof.
+  property(testName = "JSON format validation for sponsored tx") {
+    val js = Json.parse("""{
+                       "type": 20,
+                       "version": 3,
+                       "id": "DoQgKL1vp9sHgwJv3BU4FqbJVKbLHd76pnnCT5LZRin1",
+                       "sender": "3Mr31XDsqdktAdNQCdSd8ieQuYoJfsnLVFg",
+                       "senderKeyType": "ed25519",
+                       "senderPublicKey": "FM5ojNqW7e9cZ9zhPYGkpSP1Pcd8Z3e3MNKYVS5pGJ8Z",
+                       "sponsor": "3Mw6BfpSRkgCi8LQMQRKayvEb1fqKpDbaVY",
+                       "sponsorKeyType": "ed25519",
+                       "sponsorPublicKey": "22wYfvU2op1f3s4RMRL2bwWBmtHCAB6t3cRwnzRJ1BNz",
+                       "fee": 100000,
+                       "timestamp": 1526911531530,
+                       "keys": [
+                         "FM5ojNqW7e9cZ9zhPYGkpSP1Pcd8Z3e3MNKYVS5pGJ8Z"
+                       ],
+                       "proofs": [
+                         "32mNYSefBTrkVngG5REkmmGAVv69ZvNhpbegmnqDReMTmXNyYqbECPgHgXrX2UwyKGLFS45j7xDFyPXjF8jcfw94",
+                         "2z2S3W9n9AatLQ4XmR5mPfZdGY3o27JY7Bf9c7GeD3GDhGykxuSEjKMkwh2yALDcBhdduFGLT1pXJww4Dg6eMHRx"
+                       ]
+                       }
+  """)
+
+    val key = PublicKeyAccount.fromBase58String("FM5ojNqW7e9cZ9zhPYGkpSP1Pcd8Z3e3MNKYVS5pGJ8Z").explicitGet()
+    val proofs = Seq(
+      ByteStr.decodeBase58("32mNYSefBTrkVngG5REkmmGAVv69ZvNhpbegmnqDReMTmXNyYqbECPgHgXrX2UwyKGLFS45j7xDFyPXjF8jcfw94").get,
+      ByteStr.decodeBase58("2z2S3W9n9AatLQ4XmR5mPfZdGY3o27JY7Bf9c7GeD3GDhGykxuSEjKMkwh2yALDcBhdduFGLT1pXJww4Dg6eMHRx").get
+    )
+
+    val tx = RegisterTransaction
+      .create(
+        3,
+        None,
+        1526911531530L,
+        PublicKeyAccount.fromBase58String("FM5ojNqW7e9cZ9zhPYGkpSP1Pcd8Z3e3MNKYVS5pGJ8Z").explicitGet(),
+        100000,
+        List(key),
+        Some(PublicKeyAccount.fromBase58String("22wYfvU2op1f3s4RMRL2bwWBmtHCAB6t3cRwnzRJ1BNz").explicitGet()),
+        Proofs(proofs)
+      )
+      .explicitGet()
+
+    tx.json() shouldEqual js
+  }
+
+}
