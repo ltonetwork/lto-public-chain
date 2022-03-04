152cec17f3211d68f70df2f3cf22eb06d434a615
Arnold Daniels <arnold@jasny.net>
2022-03-04 11:08:50 +0100

    Generate swagger.json only once

--- src/main/scala/com/ltonetwork/api/CompositeHttpService.scala
+++ src/main/scala/com/ltonetwork/api/CompositeHttpService.scala
@@ -3,11 +3,13 @@ package com.ltonetwork.api
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model.headers._
-import akka.http.scaladsl.model.{HttpRequest, StatusCodes}
+import akka.http.scaladsl.model.{HttpEntity, HttpRequest, MediaTypes, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.RouteResult.Complete
 import akka.http.scaladsl.server.directives.{DebuggingDirectives, LoggingMagnet}
 import akka.http.scaladsl.server.{Directive0, Route, RouteResult}
+import com.github.swagger.akka.CustomMediaTypes
+import com.github.swagger.akka.SwaggerHttpService.apiDocsBase
 import com.ltonetwork.api.swagger.SwaggerDocService
 import com.ltonetwork.settings.RestAPISettings
 import com.ltonetwork.utils.ScorexLogging
@@ -22,16 +24,12 @@ case class CompositeHttpService(system: ActorSystem, apiTypes: Set[Class[_]], ro
       respondWithHeader(`Access-Control-Allow-Origin`.*)
     else pass
 
-  private val headers: scala.collection.immutable.Seq[String] = scala.collection.immutable.Seq("Authorization",
-                                                                                               "Content-Type",
-                                                                                               "X-Requested-With",
-                                                                                               "Timestamp",
-                                                                                               "Signature") ++
+  private val headers: scala.collection.immutable.Seq[String] =
+    scala.collection.immutable.Seq("Authorization", "Content-Type", "X-Requested-With", "Timestamp", "Signature") ++
     (if (settings.apiKeyDifferentHost) Seq("X-API-Key") else Seq.empty[String])
 
   val compositeRoute: Route =
     withCors(routes.map(_.route).reduce(_ ~ _)) ~
-      swaggerService.routes ~
       (pathEndOrSingleSlash | path("swagger")) {
         redirect("/api-docs/index.html", StatusCodes.PermanentRedirect)
       } ~
@@ -39,7 +37,13 @@ case class CompositeHttpService(system: ActorSystem, apiTypes: Set[Class[_]], ro
         pathEndOrSingleSlash {
           redirect("/api-docs/index.html", StatusCodes.PermanentRedirect)
         } ~
-          getFromResourceDirectory("swagger-ui")
+        path("swagger.json") {
+          complete(HttpEntity(MediaTypes.`application/json`, swaggerService.swaggerJson))
+        } ~
+        path("swagger.yaml") {
+          complete(HttpEntity(CustomMediaTypes.`text/vnd.yaml`, swaggerService.swaggerYaml))
+        } ~
+        getFromResourceDirectory("swagger-ui")
       } ~ options {
       respondWithDefaultHeaders(`Access-Control-Allow-Credentials`(true),
                                 `Access-Control-Allow-Headers`(headers),
--- src/main/scala/com/ltonetwork/api/swagger/SwaggerDocService.scala
+++ src/main/scala/com/ltonetwork/api/swagger/SwaggerDocService.scala
@@ -1,7 +1,11 @@
 package com.ltonetwork.api.swagger
 
 import akka.actor.ActorSystem
-import com.github.swagger.akka.SwaggerHttpService
+import akka.http.scaladsl.model.{HttpEntity, MediaTypes}
+import akka.http.scaladsl.server.Route
+import com.github.swagger.akka.{CustomMediaTypes, SwaggerGenerator, SwaggerHttpService}
+import com.github.swagger.akka.SwaggerHttpService.apiDocsBase
+import com.google.common.cache.{CacheBuilder, CacheLoader, LoadingCache}
 import com.ltonetwork.Version
 import com.ltonetwork.settings.RestAPISettings
 import io.swagger.v3.oas.models.info.{Info, License}
@@ -9,9 +13,13 @@ import io.swagger.v3.oas.models.security.{SecurityRequirement, SecurityScheme}
 import io.swagger.v3.oas.models.servers.Server
 import io.swagger.v3.oas.models.{Components, OpenAPI}
 
-class SwaggerDocService(val actorSystem: ActorSystem, val apiClasses: Set[Class[_]], settings: RestAPISettings) extends SwaggerHttpService {
+class SwaggerDocService(val actorSystem: ActorSystem, val apiClasses: Set[Class[_]], settings: RestAPISettings) extends SwaggerGenerator {
+  import SwaggerHttpService._
 
   override val host: String = settings.bindAddress + ":" + settings.port
+
+  lazy val swaggerJson = generateSwaggerJson
+  lazy val swaggerYaml = generateSwaggerYaml
 
   val license = new License()
   license.setName("Apache License, Version 2.0")
