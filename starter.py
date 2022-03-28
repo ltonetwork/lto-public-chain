14ffea973474e5b3d5ed49cd9484b7dff9860ac6
Arnold Daniels <arnold@jasny.net>
2022-03-28 04:52:30 +0200

    Add newline for fee-vote cron job in starter script

--- starter.py
+++ starter.py
@@ -81,7 +81,7 @@ def create_fee_vote_cron():
         return
 
     with open('/etc/cron.d/fee-vote', 'w') as f:
-        f.write('0 * * * * python /lto-node/fee-vote.py %s %s' % (node, '/lto/fee-vote'))
+        f.write('0 * * * * python /lto-node/fee-vote.py %s %s\n' % (node, '/lto/fee-vote'))
 
     os.chmod('/etc/cron.d/fee-vote', 0o644)
     os.system('crontab /etc/cron.d/fee-vote')
