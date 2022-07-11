ae5c19918ab9ca1a4f60dea6d7132f1d33e49b48
arnold <arnold@jasny.net>
2022-07-11 13:28:25 +0200

    ClaimRequest `data` field

--- src/main/scala/com/ltonetwork/api/requests/ClaimRequest.scala
+++ src/main/scala/com/ltonetwork/api/requests/ClaimRequest.scala
@@ -7,43 +7,40 @@ import com.ltonetwork.transaction.{Proofs, ValidationError}
 import play.api.libs.json.{Format, JsObject, Json}
 
 case class ClaimRequest(version: Option[Byte] = None,
-                         timestamp: Option[Long] = None,
-                         senderKeyType: Option[String] = None,
-                         senderPublicKey: Option[String] = None,
-                         fee: Long,
-                         claimType: Int,
-                         recipient: Option[String],
-                         subject: Option[ByteStr] = None,
-                         amount: Option[Long],
-                         hash: Option[ByteStr] = None,
-                         related: Option[List[ByteStr]] = None,
-                         sponsorKeyType: Option[String] = None,
-                         sponsorPublicKey: Option[String] = None,
-                         signature: Option[ByteStr] = None,
-                         proofs: Option[Proofs] = None,
-                        ) extends TxRequest.For[ClaimTransaction] {
+                        fee: Long,
+                        timestamp: Option[Long] = None,
+                        sender: Option[String] = None,
+                        senderKeyType: Option[String] = None,
+                        senderPublicKey: Option[String] = None,
+                        sponsor: Option[String] = None,
+                        sponsorKeyType: Option[String] = None,
+                        sponsorPublicKey: Option[String] = None,
+                        claimType: Int,
+                        recipient: Option[String],
+                        subject: Option[ByteStr] = None,
+                        data: Option[List[DataEntry[_]]],
+                        signature: Option[ByteStr] = None,
+                        proofs: Option[Proofs] = None)
+    extends TxRequest.For[ClaimTransaction] {
 
   protected def sign(tx: ClaimTransaction, signer: PrivateKeyAccount): ClaimTransaction = tx.signWith(signer)
 
-  def toTxFrom(sender: PublicKeyAccount, sponsor: Option[PublicKeyAccount]): Either[ValidationError, ClaimTransaction] =
+  def toTxFrom(sender: PublicKeyAccount, sponsor: Option[PublicKeyAccount], proofs: Proofs, timestamp: Long): Either[ValidationError, ClaimTransaction] =
     for {
       validRecipient <- recipient.noneIfEmpty.map(r => Address.fromString(r))
         .fold[Either[ValidationError, Option[Address]]](Right(None))(_.map(Some(_)))
-      validProofs  <- toProofs(signature, proofs)
       tx <- ClaimTransaction.create(
         version.getOrElse(ClaimTransaction.latestVersion),
         None,
-        timestamp.getOrElse(defaultTimestamp),
+        timestamp,
         sender,
         fee,
         claimType,
         validRecipient,
         subject.noneIfEmpty,
-        amount.getOrElse(0),
-        hash.noneIfEmpty,
-        related.getOrElse(List.empty[ByteStr]),
+        data.getOrElse(List.empty[DataEntry[_]]),
         sponsor,
-        validProofs
+        proofs
       )
     } yield tx
 }
